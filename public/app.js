a3af17a9152d9b6f250c63cc1c824e1b429c35a6
Anna Shelby <bonanashelby@gmail.com>
2017-03-29 16:09:26 -0700

    walk through in class

--- public/app.js
+++ public/app.js
@@ -2,7 +2,16 @@
 
 $.ajax({
   // go get the data
+  url: 'https://api.github.com/user/repos?type=owner',
+  method: 'GET',
+  headers: {
+    Authorization: `token ${githubToken}`
+  }
 })
 .then(
   // render the data
+  data => data.map(repo => $('#results').append(`<p>${repo.name}</p>`)),
+  err => {
+    console.error(err);
+  }
 );
