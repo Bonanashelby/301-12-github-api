1a8cadd5ab64d02cb85429a522804ca75a1ae934
Anna Shelby <bonanashelby@gmail.com>
2017-03-30 11:25:17 -0700

    changes for heroku

--- public/app.js
+++ public/app.js
@@ -1,17 +1,17 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
-$.ajax({
-  // go get the data
-  url: 'https://api.github.com/user/repos?type=owner',
-  method: 'GET',
-  headers: {
-    Authorization: `token ${githubToken}`
-  }
-})
+// $.ajax({
+//   // go get the data
+//   url: 'https://api.github.com/user/repos?type=owner',
+//   method: 'GET',
+//   headers: {
+//     // Authorization: `token ${githubToken}` DON'T NEED WITH HEROKU/DO NEED BEFORE HEROKU
+//     Authorization: `token ${process.env.GITHUB_TOKEN}`
+//   }
+// })
+$.get('/github/user/repos?type=owner')
 .then(
   // render the data
   data => data.map(repo => $('#results').append(`<p>${repo.name}</p>`)),
-  err => {
-    console.error(err);
-  }
+  err => console.error(err)
 );
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+// requestProxy for accessability for app.js/server.js for HEROKU? also need to npm i --save express-request-proxy in terminal to run this
+const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('./public'));
+// HEROKU
+app.get('/github/*', proxyGitHub);
+// HEROKU
+function proxyGitHub(request, response) {
+  console.log('Routing a request for a GitHub resource');
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  }))(request, response);
+}
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
